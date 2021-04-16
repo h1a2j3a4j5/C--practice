b677e1f41ad4ff761332490aff6cfa74fc47e225
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-04-16 17:13:35 +0300

    Boolean condition with functions

---
+++ or 15/or 15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace or_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3 numbers and check if all the digits in it are equal:");
+            string num1 = Convert.ToString(Console.ReadLine());
+            bool add = num(num1);
+            Console.WriteLine("Is this a correct number : {0}", add);
+        }
+        static bool num (string num1)
+        {
+            if(num1[0] == num1[1] && num1[1] == num1[2])
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
