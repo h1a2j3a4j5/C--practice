1b042742e250e835cf5574c5fadfc70e5bb05073
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-04-19 02:22:09 +0300

    Mahat test exercise 3

---
+++ Mahat test exercise 3/Mahat test exercise 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Mahat_test_exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Write a program that captures the student's shots and prints the student's best result :");
+            double ball = 0;
+            double max = 0;
+            max = ball;
+            for (int num = 1; num < 11; num++){
+                ball = Convert.ToDouble(Console.ReadLine());
+                if (ball> max)
+                {
+                    max = ball;
+                }
+            }
+            Console.WriteLine("The best result is:{0}", max);
+           
+        }
+    }
+}
