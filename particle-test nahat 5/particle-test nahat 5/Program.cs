19f1c98e00f528db2d20321abb2b89a4bfe1722c
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-04-22 13:12:55 +0300

    Amount, average, and number of days

---
+++ particle-test nahat 5/particle-test nahat 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace particle_test_nahat_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter days km:");
+            int km = 0;
+            int counter = 0;
+            int sum = 0;
+            km = int.Parse(Console.ReadLine());
+            while (km != -2)
+            {
+                counter++;
+                sum += km;
+                km = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("rusult is: {0} and avg: {1}", sum, sum / counter);
+            Console.WriteLine("The number of days he traveled: {0}", counter);
+        }
+    }
+}
