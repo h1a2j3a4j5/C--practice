84f23984b25f681c75332290892785b635f823f3
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-04-22 10:47:50 +0300

    The greatest value and its location

---
+++ or 21/or 21/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace or_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the largest value:");
+            int bigestValue = 0;
+            int location = 0;
+            for(int i = 1; i < 6; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num > bigestValue)
+                {
+                    bigestValue = num;
+                    location = i;
+                }
+            }
+            Console.WriteLine("value bigest is: {0} and His loction: {1}",bigestValue,location);
+        }
+    }
+}
