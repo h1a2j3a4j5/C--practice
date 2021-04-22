e822f3f2c8233d14970f649250068f8af26b9504
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-04-22 10:51:38 +0300

    The greatest value

---
+++ lesson 4/lesson 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lesson_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a program that captures the student's shots and prints the student's best result :");
+            int num = 1;
+            int max = 0;
+            int counter = 0;
+            
+            for (int i = 1; i < 6; i++) 
+                {
+                    num = int.Parse(Console.ReadLine());
+                max = num;
+                if (max > num)
+                    {
+                        max = i;
+                    }
+                    
+                }
+                Console.WriteLine("location: {0}", max);
+            }
+    }
+}
