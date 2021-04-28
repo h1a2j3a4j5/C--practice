9103ef85bae7df2935ad3e4d8a236c6f5c1accec
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-04-28 18:05:08 +0300

    Enter a number as soon as you enter a negative number then sum up and make an average as well

---
+++ lesson 5/lesson 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" please enter number:");
+        
+            int sum = 0;
+            int counter = 0;
+            int km = 0;
+            km = int.Parse(Console.ReadLine());
+            while (km!=-2)
+            {
+                sum += km;
+                counter++;
+                km = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("A total of a pound in ten days:{0}", sum);
+            Console.WriteLine("Average kilo meters per day:{0}", sum/counter);
+        }
+    }
+}
