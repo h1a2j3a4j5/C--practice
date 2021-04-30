e750d875650ca9ef117fdb784cc8ccecc87e4719
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-04-30 15:54:24 +0300

    ARRAY with input ( מערך )

---
+++ or 25/or 25/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace or_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter arrray: ");
+            int[] num = new int[5] ;
+             
+            int sum = 0;
+            int location = 0;
+            for(int i = 0; i < num.Length; i++)
+            {
+                
+                num[i] = int.Parse(Console.ReadLine());
+                if (num[i] >= sum)
+                {
+                    sum = num[i];
+                    location = i;
+                }
+            }
+            Console.WriteLine("number bigest at array:{0}", sum);
+            Console.WriteLine("location is:{0}",location);
+        }
+    }
+}
