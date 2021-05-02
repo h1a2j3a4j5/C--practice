aa94a8bb1bbaccf9a72ca9a711947ef5b59c56d3
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-05-02 19:11:05 +0300

    funcation with Pilndrom !

---
+++ or 26/or 26/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace or_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter number:");
+            string num = Console.ReadLine();
+            bool sum = chek(num);
+            Console.WriteLine(sum);
+
+
+
+        }
+        static bool chek(string num )
+        {
+            
+            if(num[0] == num[3] && num[1] == num[2])
+            {
+
+                Console.WriteLine("Pilndron");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Not-Pilndron");
+            }
+            return false;
+        }
+    }
+}
