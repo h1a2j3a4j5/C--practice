8a83eb5d5387b32133c52744eca74ceee6ddecf6
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-05-04 20:50:15 +0300

    A function that will receive a signal at some point and return the first / last index in its string!

---
+++ or 29/or 29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace or_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            char letter = char.Parse(Console.ReadLine());
+            int sum = add(name, letter);
+            Console.WriteLine(sum);
+        }
+        static int add (string name , char letter)
+        {
+            for (int i = 0; i <= name.Length; i++)
+            {
+                if(name[i] == letter)
+                {
+                    
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
