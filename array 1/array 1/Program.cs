2e984b391f0e2bf0d3d08269987ba9c2d73e166e
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-05-07 17:40:56 +0300

    ARRAY

---
+++ array 1/array 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[4] { 4, 6, 2, 1 };
+           
+            for(int i = 0; i < num.Length-1; i++)
+            {
+                num[i] = num[i + 1];
+            }
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+            
+        }
+    }
+}
