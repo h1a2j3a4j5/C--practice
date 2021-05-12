ce190783aec536f8307111c66e5031b8f9a833d3
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-05-12 11:49:10 +0300

    משולש

---
+++ or 33/or 33/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace or_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k, l;
+            Console.WriteLine("Please enter the range: ");
+            int n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write(k);
+                }
+                for (l = i - 1; l >= 1; l--)
+                {
+                    Console.Write(l);
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
