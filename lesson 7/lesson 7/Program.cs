5e16f566bc997f53b2f0567df7ada91ee9ae70ae
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-05-20 16:00:39 +0300

    foteach ang test of amir

---
+++ lesson 7/lesson 7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace lesson_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "erfvc";
+            string b = "feaxw";
+            string sum = "";
+
+            foreach(var l in a)
+            {
+               if (b.IndexOf(l)>=0)
+               {
+                    sum += l;
+                }
+            }
+           //Console.WriteLine(sum);
+           // string name = "mishe";
+           // int num = name.IndexOf('s');
+           // string sum = name.Substring(num);
+           // Console.WriteLine(sum);
+
+            //int[] numbers = {-2, 6, 2, 1, 3, 9, -2, 3, 73, 35,6, 55};
+            //int num = 6;
+            //int first= -1;
+            //int last = 0;
+            //for(int i = 0; i < numbers.Length; i++)
+            //{
+            //    if(num == numbers[i])
+            //    {
+            //        first = i;
+            //        break;
+            //    }
+
+            //}
+
+            //for (int i = numbers.Length-1; i >=0; i--)
+            //{
+            //    if (num == numbers[i])
+            //    {
+            //        last = i;
+            //        break;
+            //    }
+
+            //}
+            //if (numbers.Length - last <=first)
+            //{
+            //    Console.WriteLine("END");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("START");
+            //}
+
+
+        }
+    }
+}
