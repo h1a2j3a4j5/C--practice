f8f800e92dec2389050cbcf0b0d87bf185be6791
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-05-26 21:09:42 +0300

    value biggest array

---
+++ lesson 8/lesson 8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print the highest value and its location!");
+            double[] num = new double[10] { 13, 24, 12.5, 8, 24, -9, 12, 6, -37, 5 };
+            double sum = 0;
+            double location = 0;
+            
+            for(int i = 0; i <num.Length; i++)
+            {
+                if (num[i] >= sum)
+                {
+                    sum = num[i];
+                }
+            }
+
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] == sum)
+                {
+                    location = i;
+                    break;
+                }
+            }
+            Console.WriteLine("nunber is:{0} location is:{1}", sum, location);
+
+        }
+    }
+}
